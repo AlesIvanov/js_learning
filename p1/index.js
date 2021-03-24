5a6007bb377a6be7623aab1e874991b37514bf34
alesIvanov <alespolovnikov@yandex.ru>
2021-03-25 08:31:46 +1000

    ываыва

--- p1/index.js
+++ p1/index.js
@@ -1,1 +1,9 @@
 const process = require('process');
+
+// const count = process.argv.length;
+// console.log('Количество элементов:', count)
+// for (let i = 0; i < count; i++) {
+//     console.log(process.argv[i]);
+// }
+
+process.exit(1)
